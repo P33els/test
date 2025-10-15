fef4d6917f804bfd271722f656b99bf1b607dec7
Intrachai-021 <P33els>
2025-10-15 10:46:35 +0700

    second commit

--- test.py
+++ test.py
@@ -10,13 +10,16 @@ image = cv2.imread(image_filename)
 
 gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-denoised = cv2.medianBlur(gray_image, 3)
+scale_percent = 200  # เพิ่มขนาด 2 เท่า (200%)
+width = int(gray_image.shape[1] * scale_percent / 100)
+height = int(gray_image.shape[0] * scale_percent / 100)
+dim = (width, height)
+upscaled_image = cv2.resize(gray_image, dim, interpolation=cv2.INTER_CUBIC)
 
-
-text = pytesseract.image_to_string(denoised, lang='tha')
+text = pytesseract.image_to_string(upscaled_image, lang='tha')
 
 print(text)
 
-cv2.imshow("Image", denoised)
+cv2.imshow("Image", upscaled_image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
